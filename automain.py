31c05f409ea5ba70d8d8def124318bd0291441a4
Nathan West <Lucretiel@gmail.com>
2014-11-30 02:11:54 -0500

    Added license statement

--- automain.py
+++ automain.py
@@ -1,3 +1,21 @@
+# Copyright 2014-2015 Nathan West
+#
+# This file is part of automain.
+#
+# automain is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# automain is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with automain.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from inspect import signature, Parameter
 from argparse import ArgumentParser
 from contextlib import contextmanager, ExitStack
