e8f16f2ab871535f62563ac95b667c6c9c242a68
Nathan West <Lucretiel@gmail.com>
2014-11-30 01:30:37 -0500

    Another significant rewrite of automain
    - Much better _add_argument logic
    - Added autofiles
    - Added different capitalizations for -short options
    - Now attach main to main_wrapper before returning it
    - smart_open now supports str, bytes, and int, just like open
    - More comments
    - Fixed some bugs and potential exceptions

--- automain.py
+++ automain.py
@@ -1,28 +1,80 @@
 from sys import exit, argv as sys_argv
-from inspect import signature
+from inspect import signature, Parameter
 from argparse import ArgumentParser
 from functools import wraps
-from contextlib import contextmanager
+from contextlib import contextmanager, ExitStack
 from io import IOBase
 
-def _apply(result_type):
+
+_empty = Parameter.empty
+
+
+@contextmanager
+def smart_open(filename_or_file, *args, **kwargs):
     '''
-    Convert the return value of a function to a different result type. Useful
-    for making generators return containers.
+    This context manager allows you to correctly open a filename, if you want
+    to default some already-existing file object, like sys.stdout, which
+    shouldn't be closed at the end of the context. If the filename argument is
+    a str, bytes, or int, the file object is created via a call to open with
+    the given *args and **kwargs, sent to the context, and closed at the end of
+    the context, just like "with open(filename) as f". If it isn't one of the
+    openable types, the object simply sent to the context unchanged. Example:
+
+        def work_with_file(name=sys.stdout):
+            with smart_open(name) as f:
+                # Works correctly if name is a str filename or sys.stdout
+                print("Some stuff", file=f)
+                # If it was a filename, f is closed at the end here.
     '''
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            return result_type(func(*args, **kwargs))
-        return wrapper
-    return decorator
+    if isinstance(filename_or_file, (str, bytes, int)):
+        with open(filename_or_file, *args, **kwargs) as f:
+            yield f
+    else:
+        yield filename_or_file
+
+
+class _Autofile:
+    '''
+    Base class for the autofile feature. Should be instantiated with the future
+    arguments to open(). See the `autofile` function for a system to create
+    _Autofile subclasses with pre-defined args and kwargs for open(), which can
+    be instantiated with a filename.
+    '''
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+    def open(self):
+        return open(*self.args, **self.kwargs)
+
+
+def autofile(*args, **kwargs):
+    '''
+    Create an autofile type. When used by automain, autofiles are automatically
+    opened before main is called. The opened file objects are passed as
+    arguments, and automatically closed when main returns, even if it throws an
+    exception.
+
+    Of course, because the objects passed to main as arguments are normal file
+    objects, you can use your own "with" context to close the file earlier, as
+    consecutive close() calls are safe no-ops. Keep in mind, though, that the
+    standard streams probably shouldn't be closed; consider using a str type
+    and smart_open instead of autofile if you want to support manually closing
+    files in this case.
+    '''
+    class ScopedAutofile(_Autofile):
+        def __init__(self, filename):
+            super().__init__(filename, *args, **kwargs)
+    return ScopedAutofile
 
 
 def _get_type_description(annotation):
     '''
-    Given an annotation, return the type and/or description for the parameter
+    Given an annotation, return the (type, description) for the parameter
     '''
-    if isinstance(annotation, type):
+    if annotation is _empty:
+        return None, None
+    elif isinstance(annotation, type):
         return annotation, None
     elif isinstance(annotation, str):
         return None, annotation
@@ -38,13 +90,18 @@ def _get_type_description(annotation):
         annotation)
 
 
-@_apply(dict)
-def _get_arg_spec(param):
+def _add_argument(param, used_char_args):
     '''
-    For a parameter, return the kwargs to add_argument for the parameter. The
-    kwargs may contain extra implementation stuff for automain, which is popped
-    out pefore passed to add_argument
+    Get the *args and **kwargs to use for parser.add_argument for a given
+    parameter.
     '''
+    if param.kind is param.POSITIONAL_ONLY:
+        raise ValueError("parameter must have a name", param)
+    elif param.kind is param.VAR_KEYWORD:
+        raise ValueError("automain doesn't understand kwargs", param)
+
+    arg_spec = {}
+    is_option = False
 
     # Get the type and default from the annotation.
     arg_type, description = _get_type_description(param.annotation)
@@ -54,121 +111,74 @@ def _get_arg_spec(param):
 
     # If there is no explicit type, and the default is present and not None,
     # infer the type from the default.
-    if arg_type is None and param.default not in (param.empty, None):
+    if arg_type is None and default not in (_empty, None):
         arg_type = type(default)
 
-    # Special case for bool: make it just a switch
-    if arg_type is bool:
-        if default is not param.empty:
-            yield 'default', default
+    # Add the type
+    if arg_type is not None:
+        # Special case for bool: make it just a --switch
+        if arg_type is bool:
+            if not default or default is _empty:
+                arg_spec['action'] = 'store_true'
+            else:
+                arg_spec['action'] = 'store_false'
 
-        yield '_option', True
-        if not default or default is param.empty:
-            yield 'action', 'store_true'
+            # Make it an option even if there's no explicit default
+            is_option = True
+
+        # Special case for file object types: make it a string type, for filename
+        elif issubclass(arg_type, IOBase):
+            arg_spec['type'] = str
+
+        # Everything else: make it a plain type
         else:
-            yield 'action', 'store_false'
-
-    # Special case or IOBase: make it a string, for a filename.
-    elif isinstance(default, IOBase):
-        yield 'default', default
-        if issubclass(arg_type, IOBase):
-            yield 'type', str
-        else:
-            yield 'type', arg_type
-        yield '_option', True
-
-    # Everything else: yield type and default
-    else:
-        if default is not param.empty:
-            yield '_option', True
-            yield 'default', default
-        if arg_type is not None:
-            yield 'type', arg_type
+            arg_spec['type'] = arg_type
 
     # nargs: if the signature includes *args, collect them as trailing CLI
     # arguments in a list.
     if param.kind is param.VAR_POSITIONAL:
-        yield 'nargs', '*'
+        # TODO: consider depluralizing metavar/name here.
+        arg_spec['nargs'] = '*'
 
-    # DESCRIPTION
+    # Add description.
     if description is not None:
-        yield 'help', description
+        arg_spec['help'] = description
+
+    # Add default. The presence of a default means this is an option, not an
+    # argument.
+    if default is not _empty:
+        arg_spec['default'] = default
+        is_option = True
+
+    # Get the --flags
+    flags = []
+    name = param.name
+
+    if is_option:
+        # Add the first letter as a -short option. Attempt to add -c or -C,
+        # trying various capitalizations.
+        for letter in name[0], name[0].upper(), name[0].lower():
+            if letter not in used_char_args:
+                used_char_args.add(letter)
+                flags.append('-{}'.format(letter))
+                break
+
+        # If the function argument only had one letter, and it could be added
+        # as a -short option, don't bother adding a --long option
+        if not (len(name) == 1 and flags):
+            flags.append('--{}'.format(name))
+
+        # Add an explicit dest, in case the name was converted
+        arg_spec['dest'] = name
+    else:
+        flags.append(name)
 
     # TODO: special case for list type.
     #   - How to specificy type of list members?
     #   - action='append' vs nargs='*'
+    # TODO: Update bool case to suppose --no-option, as a counter to --option
 
-    # TODO: special case for default=sys.stdin and friends.
-    #   - type - should be str, something else?
-    #   - check for file existence?
-    #   - find a way to make it an argument instead of an option
-    #   - how to allow for explicit type if provided, but not use type(default)
-
-
-@_apply(tuple)
-def _get_arg_flags(name, is_option, used_char_args):
-    '''
-    For a parameter, get the flags to add_argument for the parameter.
-
-    name: the name of the argument
-    is_option: if True, will return -o and --option flags.
-    used_char_args: set of characters used to prevent reuse of -o options.
-    '''
-    # NOTE: it is critical that the logic for this function be deterministic.
-    # Be careful when using sets or dicts
-
-    # TODO: smarter logic: allow for --config/-C, etc. This will require
-    # figuring out the different permutations of flag/dest/metavar.
-    if is_option:
-        if name[0] not in used_char_args:
-            used_char_args.add(name[0])
-            yield '-{}'.format(name[0])
-        yield '--{}'.format(name)
-    else:
-        yield name
-
-
-def _add_argument(parser, param, used_char_args):
-    if param.kind is param.POSITIONAL_ONLY:
-        raise ValueError("parameter must have a name", param)
-    elif param.kind is param.VAR_KEYWORD:
-        raise ValueError("automain doesn't understand kwargs", param)
-
-    arg_spec = _get_arg_spec(param)
-
-    flags = _get_arg_flags(
-        param.name,
-        arg_spec.pop('_option', False),
-        used_char_args)
-
-    return parser.add_argument(*flags, **arg_spec)
-
-
-def _add_arguments(parser, params):
-    used_char_args = {'h'}
-
-    for param in params.values():
-        _add_argument(parser, param, used_char_args)
-
-
-@contextmanager
-def smart_open(filename_or_file, *args, **kwargs):
-    '''
-    This context manager allows you to correctly open a filename, if there's a
-    chance you want some already-existing file object, like sys.stdout. If the
-    filename argument is a str, the file is opened, sent to the context, and
-    closed at the end of the context, just like "with open(filename) as f". If
-    it isn't a str, the object simply sent to the context unchanged. Example:
-
-        def work_with_file(name=sys.stdout):
-            with smart_open(name) as f:
-                print("Some stuff", file=f)
-    '''
-    if isinstance(filename_or_file, str):
-        with open(filename_or_file, *args, **kwargs) as f:
-            yield f
-    else:
-        yield filename_or_file
+    return flags, arg_spec
 
 
 def automain(module, description=None, epilog=None):
@@ -185,28 +195,57 @@ def automain(module, description=None, epilog=None):
 
     If `module` == "__main__", the decorated function is called immediately
     with sys.argv, and the progam is exited with the return value; this is so
-    that you can call @automain(__name__) and be able to import the module for
-    testing.
+    that you can call @automain(__name__) and still be able to import the
+    module for testing.
 
-    The decorated function is attached to the result as the `main` attribute. False
+    The decorated function is attached to the result as the `main` attribute.
     '''
     def decorator(main):
         parser = ArgumentParser(description=description, epilog=epilog)
         main_sig = signature(main)
-        _add_arguments(parser, main_sig.parameters)
+
+        used_char_args = {'h'}
+        # Add each argument. Do single-character arguments first, if present,
+        # so that they get priority, and don't have to get --long versions.
+        # sorted is stable, so the parameters will still be in relative order.
+        for param in sorted(main_sig.parameters.values(),
+                key=lambda param: len(param.name) > 1):
+            flags, spec = _add_argument(param, used_char_args)
+            parser.add_argument(*flags, **spec)
 
         def main_wrapper(argv):
+            # Update parser with program name
             parser.prog = argv[0]
+
+            # Parse arguments
+            args = vars(parser.parse_args(argv[1:]))
+
+            # Get empty argument binding
             function_args = main_sig.bind_partial()
-            function_args.arguments.update(vars(parser.parse_args(argv[1:])))
-            return main(*function_args.args, **function_args.kwargs)
 
-        main_wrapper.main = main
+            # Context for autofiles
+            with ExitStack() as stack:
+                # Open autofiles
+                for arg_name, arg_value in args.items():
+                    if isinstance(arg_value, _Autofile):
+                        args[arg_name] = stack.enter_context(arg_value.open())
 
+                # TODO: do something smarter if the file can't be opened
+
+                # Apply command line arguments to function arguments
+                function_args.arguments.update(args)
+
+                # Call main function
+                return main(*function_args.args, **function_args.kwargs)
+
+        # If we are running as a script/program, call main right away, then exit
         if module == '__main__':
             exit(main_wrapper(sys_argv))
 
+        # Otherwise, attach the wrapped main function, and return the wrapper.
+        main_wrapper.main = main
         return main_wrapper
+
     return decorator
 
 
