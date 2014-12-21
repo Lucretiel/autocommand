9ece4822293105794891d7486ca8a8745af3fac1
Nathan West <Lucretiel@gmail.com>
2014-12-21 00:47:46 -0500

    Updated comments for smart_open

--- automain.py
+++ automain.py
@@ -224,13 +224,13 @@ def automain(module=None, *, description=None, epilog=None, add_nos=False):
 @contextmanager
 def smart_open(filename_or_file, *args, **kwargs):
     '''
-    This context manager allows you to correctly open a filename, if you want
-    to default some already-existing file object, like sys.stdout, which
-    shouldn't be closed at the end of the context. If the filename argument is
-    a str, bytes, or int, the file object is created via a call to open with
-    the given *args and **kwargs, sent to the context, and closed at the end of
-    the context, just like "with open(filename) as f". If it isn't one of the
-    openable types, the object simply sent to the context unchanged. Example:
+    This context manager allows you to open a filename, if you want to default
+    some already-existing file object, like sys.stdout, which shouldn't be
+    closed at the end of the context. If the filename argument is a str, bytes,
+    or int, the file object is created via a call to open with the given *args
+    and **kwargs, sent to the context, and closed at the end of the context,
+    just like "with open(filename) as f:". If it isn't one of the openable
+    types, the object simply sent to the context unchanged. Example:
 
         def work_with_file(name=sys.stdout):
             with smart_open(name) as f:
