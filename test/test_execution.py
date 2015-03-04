2c1ab06bc5514663ef48157ecdab632a90a94d30
Lucretiel <Lucretiel@users.noreply.github.com>
2015-03-04 10:33:25 -0500

    Fixed tab indentation

--- test/test_execution.py
+++ test/test_execution.py
@@ -4,20 +4,20 @@ from autocommand import autocommand
 
 
 @pytest.mark.parametrize('module_name', [
-	'__main__',
-	True])
+    '__main__',
+    True])
 def test_name_main(capsys, module_name):
-	try:
-		with patch('sys.argv', ['prog', 'foo', 'bar']):
-			@autocommand(module_name)
-			def prog(arg1, arg2):
-				print(arg1, arg2)
-				return 10
+    try:
+        with patch('sys.argv', ['prog', 'foo', 'bar']):
+            @autocommand(module_name)
+            def prog(arg1, arg2):
+                print(arg1, arg2)
+                return 10
 
-	except SystemExit as e:
-		assert e.code == 10
-	else:
-		pytest.fail('autocommand function failed to raise SystemExit')
+    except SystemExit as e:
+        assert e.code == 10
+    else:
+        pytest.fail('autocommand function failed to raise SystemExit')
 
-	out, err = capsys.readouterr()
-	assert out == "foo bar\n"
+    out, err = capsys.readouterr()
+    assert out == "foo bar\n"
