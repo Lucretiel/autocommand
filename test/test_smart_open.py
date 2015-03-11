d99fb35316f62aaeef4b8953b73be2ea3d949258
Nathan West <Lucretiel@gmail.com>
2015-03-10 22:33:41 -0400

    Added smart_open flush test

--- test/test_smart_open.py
+++ test/test_smart_open.py
@@ -1,5 +1,5 @@
 from autocommand import smart_open
-from pytest import raises, fixture, yield_fixture
+from pytest import raises, fixture, yield_fixture, mark
 from unittest.mock import patch, MagicMock
 from io import IOBase
 
@@ -19,30 +19,28 @@ def test_path():
     '''
     with patch_open() as mock_open:
         with smart_open('/path/to/file') as file:
-            file.readline()
+            mock_open.assert_called_once_with('/path/to/file')
+            file.__enter__.assert_called_once_with()
 
-    mock_open.assert_called_once_with('/path/to/file')
-    file.__enter__.assert_called_once_with()
-    file.readline.assert_called_once_with()
-    file.__exit__.assert_called_once_with(None, None, None)
+        file.__exit__.assert_called_once_with(None, None, None)
 
-
-def test_file():
+@mark.parametrize('flush', [True, False])
+def test_file(flush):
     '''
     Test smart_open with NOT a file path. The input object should be returned,
-    and no context methods should be called on it
+    and it should be flushed after
     '''
     fakefile = mockfile()
     # For some reason open(MagicMock()) doesn't throw, so we have to mock open
 
     with patch_open(side_effect=TypeError):
-        with smart_open(fakefile) as file:
-            file.readline()
+        with smart_open(fakefile, flush=flush) as file:
+            assert file is fakefile
+            assert not fakefile.__enter__.called
 
-    assert file is fakefile
-    assert not file.__enter__.called
-    file.readline.assert_called_once_with()
-    assert not file.__enter__.called
+        assert not fakefile.__exit__.called
+        if flush:
+            fakefile.flush.assert_called_once_with()
 
 
 def test_open_args(tmpdir):
